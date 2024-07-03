dbaf7f77db401c5c02a25998e69cb3b7c7165b86
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-07-03 14:24:05 +0200

    add tabs

--- App.tsx
+++ App.tsx
@@ -1,32 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black',
   },
   customText: {
-    color: 'black',
+    color: 'teal',
     fontSize: 30,
     fontFamily: 'AvenirLTProRoman',
   },
 });
 
-function App(): React.JSX.Element {
+function DummyFumc() {
   return (
     <View style={styles.main}>
-      <Text>Hello Lindo To React Native</Text>
+      <Text style={styles.customText}>Hello Lindo To React Native</Text>
     </View>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={DummyFumc} />
+        <Tab.Screen name="Settings" component={DummyFumc} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
